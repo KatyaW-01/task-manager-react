752fae806a6d9620779ede0e29425b536798f3a2
Katya Weicht <katya.weicht@gmail.com>
2025-05-18 13:41:46 -0600

    Add functions to form component to submit new tasks

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,45 @@
 import { useState } from "react";
 import "./Form.css";
 
-function Form() {
+function Form({ addTask }) {
   const [title, setTitle] = useState("");
-  const [description,setDescription] = useState("")
+  const [description, setDescription] = useState("")
+
+  function submitTasks(event) {
+    event.preventDefault();
+    const newTask = {
+      title,
+      description,
+    };
+
+    addTask(newTask);
+    clearInput()
+  }
+
+  function clearInput() {
+    setTitle("")
+    setDescription("")
+  }
 
   return (
     <form>
-      <input type="text" placeholder="Title" name="title" value={title} />
+      <input 
+        type="text" 
+        placeholder="Title" 
+        name="title" 
+        value={title}
+        onChange = {(event) => setTitle(event.target.value)} 
+      />
 
-      <input type="text" placeholder="Description" name="description" value={description} />
+      <input 
+        type="text" 
+        placeholder="Description" 
+        name="description" 
+        value={description} 
+        onChange={(event) => setDescription(event.target.value)}
+      />
 
-      <button>SUBMIT</button>
+      <button onClick={(event) => submitTasks(event)}>SUBMIT</button>
     </form>
   );
 }
